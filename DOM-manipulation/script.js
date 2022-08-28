53cd643cd9cd6655c31721c64dd4083a37db808b
Dillon Tan Zhao Kai <dillon.tanzhaokai@gmail.com>
2022-08-28 22:22:46 +0800

    Add 3rd styling and id to the div

--- DOM-manipulation/script.js
+++ DOM-manipulation/script.js
@@ -7,4 +7,8 @@ const secondText = document.createElement('h3');
     secondText.textContent = 'I\'m a blue h3!';
     secondText.style.color = 'blue';
     container.appendChild(secondText);
-    
+
+const thirdStyle = document.createElement('div');
+    thirdStyle.setAttribute('style', 'background-color: pink; border: 1px solid black');
+    thirdStyle.setAttribute('id', 'container2');
+    container.appendChild(thirdStyle);
