380c22992de95247eb4857424126ce0e58a6552c
Dillon Tan Zhao Kai <dillon.tanzhaokai@gmail.com>
2022-08-28 22:19:08 +0800

    Add 2nd styling - blue h3 with text

--- DOM-manipulation/script.js
+++ DOM-manipulation/script.js
@@ -1,6 +1,10 @@
 const firstText = document.createElement('p');
+    firstText.textContent = "Hey I'm red!";
+    firstText.style.color = 'red';
+    container.appendChild(firstText);
 
-firstText.textContent = "Hey I'm red!";
-firstText.style.color = 'red';
-
-container.appendChild(firstText);
+const secondText = document.createElement('h3');
+    secondText.textContent = 'I\'m a blue h3!';
+    secondText.style.color = 'blue';
+    container.appendChild(secondText);
+    
